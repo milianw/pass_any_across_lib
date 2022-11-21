b5971eeb70604157060c3db988ced574d3c5e83d
Nathan Collins <nathan.collins@kdab.com>
2022-11-21 16:04:04 +0000

    Add failure case for macOS
    The failure only occurs when using QSharedPointer.
    
    ```
    test 1
        Start 1: pass_any_across_lib
    
    1: Test command: /Users/nathan/KDAB/devel/edu/pass_any_across_lib/build-local-dev-qt/pass_any_across_lib
    1: Working Directory: /Users/nathan/KDAB/devel/edu/pass_any_across_lib/build-local-dev-qt
    1: Test timeout computed to be: 10000000
    1: libc++abi: terminating with uncaught exception of type std::bad_any_cast: bad any cast
    1: genAny:      name=14QSharedPointerIiE        hash_code=4372004276
    1: main:        name=14QSharedPointerIiE        hash_code=4372004276
    1: typeid matches:      0
    1/1 Test #1: pass_any_across_lib ..............Subprocess aborted***Exception:   0.49 sec
    libc++abi: terminating with uncaught exception of type std::bad_any_cast: bad any cast
    genAny: name=14QSharedPointerIiE        hash_code=4372004276
    main:   name=14QSharedPointerIiE        hash_code=4372004276
    typeid matches: 0
    
    0% tests passed, 1 tests failed out of 1
    
    Total Test time (real) =   0.49 sec
    ```

--- genany.cpp
+++ genany.cpp
@@ -1,11 +1,20 @@
 #include "genany.h"
 
-#include <memory>
 #include <iostream>
+
+#if defined(USE_QT)
+#include <QSharedPointer>
+#else
+#include <memory>
+#endif
 
 std::any genAny()
 {
+#if defined(USE_QT)
+    auto ret = std::any(QSharedPointer<int>::create(42));
+#else
     auto ret = std::any(std::make_shared<int>(42));
+#endif
     std::cout << "genAny:\tname=" << ret.type().name() << "\thash_code=" << ret.type().hash_code() << '\n';
     return ret;
 }
